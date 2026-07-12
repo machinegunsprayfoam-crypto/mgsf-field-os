a58685185fe5d4be43a697a321eb9919c71ee963
machinegunsprayfoam-crypto <machinegunsprayfoam@gmail.com>
2026-07-12 16:10:32 +0000

    fix lint configuration for next 16 and eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/purity": "warn",
+      "@next/next/no-html-link-for-pages": "warn",
+      "react/no-unescaped-entities": "warn",
+    },
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
